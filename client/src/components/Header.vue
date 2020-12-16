bcfb7bcb0090b3cc5120493c3b2adb6a8a50d4a2
hienviluong125 <hienviluong125@gmail.com>
2020-12-16 18:18:37 +0700

    implement basic home page and navbar menu

---
+++ client/src/components/Header.vue
@@ -0,0 +1,70 @@
+<template>
+  <b-navbar class="bg-white" toggleable="lg" type="light" variant="light">
+    <b-container>
+      <b-navbar-brand to="/">ZeroBugs</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item class="ml-4" to="/locations">
+            <div class="text-dark">Locations</div>
+          </b-nav-item>
+          <b-nav-item class="ml-4" to="/workspaces">
+            <div class="text-dark">Workspaces</div>
+          </b-nav-item>
+          <b-nav-item class="ml-4" to="/enterprise">
+            <div class="text-dark">Enterprise</div>
+          </b-nav-item>
+          <b-nav-item class="ml-4" to="/ideas">
+            <div class="text-dark">Ideas</div>
+          </b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item class="ml-4" to="/contact">
+            <div class="text-dark">Contact us</div>
+          </b-nav-item>
+
+          <b-nav-item-dropdown v-if="currentUser" right class="ml-4">
+            <!-- Using 'button-content' slot -->
+            <template #button-content>
+              <b-icon icon="person" font-scale="1" variant="dark" />
+              &ensp;
+              <span class="text-dark">User</span>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item v-else class="ml-4" to="/login">
+            <div class="text-dark">
+              <b-icon icon="person" font-scale="1" variant="dark" />
+              &ensp;
+              Login
+            </div>
+          </b-nav-item>
+
+        </b-navbar-nav>
+      </b-collapse>
+    </b-container>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {
+      currentUser: null
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .nav-item-text > a {
+    color: #000000;
+  }
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,10 +1,98 @@
 <template>
-  <h1>Home page</h1>
+  <b-container class="mt-3">
+    <b-row>
+      <b-col lg="2" md="12" sm="12" xs="12">
+        <div class="find-workspace-form bg-white rounded p-3 m-auto">
+          <h3>
+            Reimagine your workspace
+          </h3>
+          <p class="mt-4">
+            Whether you’re an established enterprise or a growing startup, discover flexible spaces and solutions to move your business forward.
+          </p>
+          <b-form>
+            <b-form-group id="input-group-3" label="Find workspace in" label-for="location-input">
+              <b-form-select
+                id="location-input"
+                v-model="findLocationForm.location"
+                :options="locations"
+                required
+              ></b-form-select>
+            </b-form-group>
+
+            <b-button type="submit" class="w-100" variant="primary">Start</b-button>
+          </b-form>
+        </div>
+      </b-col>
+      <b-col lg="10" md="12" sm="12" xs="12">
+        <div>
+          <b-carousel
+            v-model="slide"
+            :interval="4000"
+            controls
+            img-width="1024"
+            img-height="480"
+            @sliding-start="onSlideStart"
+            @sliding-end="onSlideEnd"
+          >
+            <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=52" />
+            <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54" />
+            <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
+            <b-carousel-slide>
+              <template #img>
+                <img
+                  class="d-block img-fluid w-100"
+                  width="1024"
+                  height="480"
+                  src="https://picsum.photos/1024/480/?image=55"
+                  alt="image slot"
+                >
+              </template>
+            </b-carousel-slide>
+          </b-carousel>
+        </div>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
-<script>
 
+
+<script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  data() {
+    return {
+      findLocationForm: {
+        location: 'TPHCM'
+      },
+      locations: ['TPHCM', 'HN', 'ĐN']
+    }
+  }
 }
 </script>
+
+<style scoped>
+  .find-workspace-form {
+    width: 300px;
+    min-width: 300px;
+    height: auto;
+    top: 25px;
+    z-index: 2;
+  }
+
+  /* Medium devices (tablets, 768px and up) The navbar toggle appears at this breakpoint */
+  @media (min-width: 576px) {
+    .find-workspace-form {
+      position: relative;
+      min-width: 400px;
+    }
+  }
+
+  @media (min-width: 992px) {
+    .find-workspace-form {
+      position: absolute;
+      min-width: 300px;
+    }
+  }
+
+</style>
