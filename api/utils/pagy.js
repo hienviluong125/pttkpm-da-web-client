45fef47165cdbe0092863b0391312015cc79be2a
hienviluong125 <hienviluong125@gmail.com>
2021-01-30 21:18:31 +0700

    implement workspace CRUD

---
+++ api/utils/pagy.js
@@ -0,0 +1,33 @@
+module.exports = async function ({
+  model,
+  queryOption,
+  pageParams,
+  limit,
+  page,
+  include = [],
+  order = [['id', 'DESC']]
+}) {
+  const offset = limit * (page - 1);
+  const queryRs = await model.findAndCountAll({
+    where: queryOption,
+    limit: limit,
+    offset: offset,
+    include: include,
+    order: order
+  });
+
+  const count = queryRs.count;
+  const firstPage = page === 1;
+  const lastPage = offset + limit >= count;
+
+  return {
+    data: queryRs.rows,
+    firstPage: firstPage,
+    count: count,
+    currentPage: page,
+    lastPage: lastPage,
+    prevPage: firstPage ? -1 : page - 1,
+    nextPage: lastPage ? -1 : page + 1,
+    totalPage: Math.ceil(count / limit)
+  }
+}
